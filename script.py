b1bc0ac9b58dcf87a2cbc3e3d034ada949436087
Sibian DG <sibiandegussem@outlook.com>
2022-01-30 16:08:01 +0100

    Printer better the titles and links + option to print filenames

--- script.py
+++ script.py
@@ -4,14 +4,16 @@ from selenium.webdriver.common.by import By
 import re
 
 # link = input('Give your link to download podcasts: ')
-link = 'https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5zb3VuZGNsb3VkLmNvbS91c2Vycy9zb3VuZGNsb3VkOnVzZXJzOjQ3MzQ4NjczOS9zb3VuZHMucnNz'
+link = 'https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5jYXB0aXZhdGUuZm0vc2xpbW1lci1wcmVzdGVyZW4v?sa=X&ved=2ahUKEwjF6dC52tn1AhWKg_0HHcaBCzgQ9sEGegQIARAE'
 episodes = {}
+filenames = {}
 options = Options()
 options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(options=options)
 try:
     # Navigate to url
     driver.get(link)
+    driver.save_screenshot('test.png')
     elements = driver.find_elements_by_class_name('oD3fme')
     for e in elements:
         title = e.find_elements_by_class_name('e3ZUqe')[0].get_attribute('innerHTML')
@@ -19,12 +21,19 @@ try:
         for t in tempEl[0].find_elements_by_tag_name('div'):
             if t.get_attribute('jsdata'):
                 jsdata = t.get_attribute('jsdata')
-                print(jsdata)
-                match = re.search("https://[^;]+\.mp3|http://www\.podtrac\.com/pts/redirect\.mp3/[^;]+\.mp3", jsdata)
-                episodes[title] = match.group()
-                print('added: ' + episodes[title])
+                link = re.search("https://[^;]+\.mp3|http://www\.podtrac\.com/pts/redirect\.mp3/[^;]+\.mp3", jsdata).group()
+                filename = re.search("[^/]+\.mp3", jsdata).group()
+                episodes[title] = link
+                filenames[filename] = link
+                # print('added: ' + episodes[title])
 finally:
     driver.quit()
-    print(episodes)
 
+    print("Title -> link")
+    for key, value in episodes.items():
+        print(f"{key} -> {value}")
+
+    print("\n\nFilename -> link")
+    for key, value in filenames.items():
+        print(f"{key} -> {value}")
 
--- selenium.md
+++ selenium.md
@@ -1,6 +1,8 @@
 https://github.com/mozilla/geckodriver/releases
 
-https://sites.google.com/a/chromium.org/chromedriver/downloads
 https://sites.google.com/chromium.org/driver/
+--> Preferred
 
 copy to: C:\Users\sibia\AppData\Local\Programs\Python\Python37-32
+or C:\tools\Anaconda3\
+
